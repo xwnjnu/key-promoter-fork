f3947de502b23219687be0796035ce25e2d2e584
Aimo Thiele <aimo.thiele@coremedia.com>
2015-01-10 20:45:43 +0100

    #4 A better tool window created with the UI designer

---
+++ src/org/jetbrains/contest/keypromoter/KeyPromoterToolWindowBuilder.java
@@ -0,0 +1,77 @@
+package org.jetbrains.contest.keypromoter;
+
+import com.intellij.openapi.components.ServiceManager;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by athiele on 10.01.2015.
+ */
+public class KeyPromoterToolWindowBuilder {
+
+  private KeyPromoterPersistentStats statsService = ServiceManager.getService(KeyPromoterPersistentStats
+          .class);
+
+  private JButton refreshButton;
+  private JPanel panel;
+  private JList list1;
+  private String[] topTen = new String[10];
+
+  public KeyPromoterToolWindowBuilder() {
+    refreshButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        updateTopTen();
+      }
+    });
+  }
+
+  public JPanel createToolWindowPanel() {
+    updateTopTen();
+    return panel;
+  }
+
+
+  private void updateTopTen() {
+    Map<String, Integer> stats = statsService.getStats();
+    List<Map.Entry<String, Integer>> sortedEntries = sortByValues(stats);
+
+    int i = 0;
+    for (Map.Entry entry : sortedEntries) {
+      topTen[i] = "[" + (i+1) + "] " + entry.getValue() + " times: " + entry.getKey();
+      i++;
+      if (i == 10) break;
+    }
+
+    // put the data into the list
+    list1.setListData(topTen);
+  }
+
+  // Sort function
+  static <K,V extends Comparable<? super V>>
+  List<Map.Entry<K, V>> sortByValues(Map<K,V> map) {
+
+    List<Map.Entry<K,V>> sortedEntries = new ArrayList<Map.Entry<K,V>>(map.entrySet());
+
+    Collections.sort(sortedEntries,
+            new Comparator<Map.Entry<K, V>>() {
+              @Override
+              public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+                return e2.getValue().compareTo(e1.getValue());
+              }
+            }
+    );
+
+    return sortedEntries;
+  }
+
+}
--- src/org/jetbrains/contest/keypromoter/KeyPromoterToolWindowFactory.java
+++ src/org/jetbrains/contest/keypromoter/KeyPromoterToolWindowFactory.java
@@ -1,105 +1,31 @@
 package org.jetbrains.contest.keypromoter;
 
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.components.JBList;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Created by athiele on 05.01.2015.
  */
 public class KeyPromoterToolWindowFactory implements ToolWindowFactory {
 
-  private JButton refreshButton = new JButton("Refresh");
-  private KeyPromoterPersistentStats statsService = ServiceManager.getService(KeyPromoterPersistentStats
-          .class);
 
-  private JLabel headline = new JLabel("Key Promoter TOP TEN:");
-  private JPanel toolWindowContent = new JPanel();
-  private JList topTenList;
-  private String[] topTen = new String[10];
+  private KeyPromoterToolWindowBuilder toolWindowBuilder = new KeyPromoterToolWindowBuilder();
+  private ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();;
 
-  public KeyPromoterToolWindowFactory() {
-
-    refreshButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        updateStats();
-        topTenList.setListData(topTen);
-      }
-    });
-  }
-  
   @Override
   public void createToolWindowContent(Project project, ToolWindow toolWindow) {
 
-    updateStats();
-
-    ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-
-
-    toolWindowContent.setLayout(new GridLayout(3, 1));
-    toolWindowContent.add(headline);
-    topTenList = new JBList(topTen);
-    topTenList.setFixedCellHeight(14);
-
-    toolWindowContent.add(topTenList);
-    toolWindowContent.add(refreshButton);
-
-    Content content = contentFactory.createContent(toolWindowContent, "", false);
+    JPanel toolWindowContent = toolWindowBuilder.createToolWindowPanel();
+    Content content = contentFactory.createContent(toolWindowContent, "Top Ten", false);
     toolWindow.getContentManager().addContent(content);
-    
-  }
-
-  private void updateStats() {
-    Map<String, Integer> stats = statsService.getStats();
-    List<Entry<String, Integer>> sortedEntries = sortByValues(stats);
-
-    int i = 0;
-    for (Entry entry : sortedEntries) {
-
-      topTen[i] = "[" + (i+1) + "] " + entry.getValue() + " times: " + entry.getKey();
-
-      i++;
-      if (i == 10) break;
-
-    }
 
   }
 
-  static <K,V extends Comparable<? super V>>
-  List<Entry<K, V>> sortByValues(Map<K,V> map) {
-
-    List<Entry<K,V>> sortedEntries = new ArrayList<Entry<K,V>>(map.entrySet());
-
-    Collections.sort(sortedEntries,
-            new Comparator<Entry<K, V>>() {
-              @Override
-              public int compare(Entry<K, V> e1, Entry<K, V> e2) {
-                return e2.getValue().compareTo(e1.getValue());
-              }
-            }
-    );
-
-    return sortedEntries;
-  }
 
 
 }
