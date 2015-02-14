b9bbfb4755fb39c125d1286a3449e73427f8374d
Aimo Thiele <aimo.thiele@coremedia.com>
2015-02-14 20:19:05 +0100

    #6 provide a reset stats button

--- src/org/jetbrains/contest/keypromoter/KeyPromoterToolWindowBuilder.java
+++ src/org/jetbrains/contest/keypromoter/KeyPromoterToolWindowBuilder.java
@@ -7,71 +7,81 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.awt.event.MouseAdapter;
+import java.util.*;
 
 /**
  * Created by athiele on 10.01.2015.
  */
 public class KeyPromoterToolWindowBuilder {
 
-  private KeyPromoterPersistentStats statsService = ServiceManager.getService(KeyPromoterPersistentStats
-          .class);
+    private KeyPromoterPersistentStats statsService = ServiceManager.getService(KeyPromoterPersistentStats
+            .class);
 
-  private JButton refreshButton;
-  private JPanel panel;
-  private JList list1;
-  private String[] topTen = new String[10];
+    private JButton refreshButton;
+    private JPanel panel;
+    private JList list1;
+    private JButton resetStatisticsButton;
+    private String[] topTen = new String[10];
 
-  public KeyPromoterToolWindowBuilder() {
-    refreshButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        updateTopTen();
-      }
-    });
-  }
-
-  public JPanel createToolWindowPanel() {
-    updateTopTen();
-    return panel;
-  }
-
-
-  private void updateTopTen() {
-    Map<String, Integer> stats = statsService.getStats();
-    List<Map.Entry<String, Integer>> sortedEntries = sortByValues(stats);
-
-    int i = 0;
-    for (Map.Entry entry : sortedEntries) {
-      topTen[i] = "[" + (i+1) + "] " + entry.getValue() + " times: " + entry.getKey();
-      i++;
-      if (i == 10) break;
+    public KeyPromoterToolWindowBuilder() {
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateTopTen();
+            }
+        });
+        resetStatisticsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetStats();
+            }
+        });
     }
 
-    // put the data into the list
-    list1.setListData(topTen);
-  }
+    public JPanel createToolWindowPanel() {
+        updateTopTen();
+        return panel;
+    }
 
-  // Sort function
-  static <K,V extends Comparable<? super V>>
-  List<Map.Entry<K, V>> sortByValues(Map<K,V> map) {
+    private void resetStats() {
+        Map<String, Integer> stats = statsService.getStats();
+        stats.clear();
+        list1.setListData(new String[10]);
+    }
 
-    List<Map.Entry<K,V>> sortedEntries = new ArrayList<Map.Entry<K,V>>(map.entrySet());
 
-    Collections.sort(sortedEntries,
-            new Comparator<Map.Entry<K, V>>() {
-              @Override
-              public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
-                return e2.getValue().compareTo(e1.getValue());
-              }
-            }
-    );
+    private void updateTopTen() {
+        Map<String, Integer> stats = statsService.getStats();
+        List<Map.Entry<String, Integer>> sortedEntries = sortByValues(stats);
 
-    return sortedEntries;
-  }
+        int i = 0;
+        for (Map.Entry entry : sortedEntries) {
+            topTen[i] = "[" + (i + 1) + "] " + entry.getValue() + " times: " + entry.getKey();
+            i++;
+            if (i == 10) break;
+        }
+
+        // put the data into the list
+        list1.setListData(topTen);
+    }
+
+    // Sort function
+    static <K, V extends Comparable<? super V>>
+    List<Map.Entry<K, V>> sortByValues(Map<K, V> map) {
+
+        List<Map.Entry<K, V>> sortedEntries = new ArrayList<Map.Entry<K, V>>(map.entrySet());
+
+        Collections.sort(sortedEntries,
+                new Comparator<Map.Entry<K, V>>() {
+                    @Override
+                    public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+                        return e2.getValue().compareTo(e1.getValue());
+                    }
+                }
+        );
+
+        return sortedEntries;
+    }
 
 }
